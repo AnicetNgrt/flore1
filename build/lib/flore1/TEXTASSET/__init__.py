64ad190611d9d68837b2a88b254c48fc4297e5fa
AnicetNgrt <an.nougaret@gmail.com>
2019-10-20 11:48:05 +0200

    removed useless dirs

--- build/lib/flore1/TEXTASSET/__init__.py
+++
@@ -1,9 +0,0 @@
-"""/////////////////////////
-///
-///   File: TEXTASSET/__init__.py
-///   Author: Anicet Nougaret
-///   License: CC BY-SA (see FLORE1/license.txt)
-///
-/////////////////////////"""
-
-from .textAsset import *
--- build/lib/flore1/VIRTUALSCENE/__init__.py
+++
@@ -1,9 +0,0 @@
-"""/////////////////////////
-///
-///   File: VIRTUALSCENE/__init__.py
-///   Author: Anicet Nougaret
-///   License: CC BY-SA (see FLORE1/license.txt)
-///
-/////////////////////////"""
-
-from .virtualScene import *
