1f2ad57a136886ef74015d3bd60c093289bd1bed
ANICET NOUGARET <nougaret@iut.univ-paris5.fr>
2019-10-24 10:12:36 +0200

    anging version to 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="flore1",
-    version="0.0.1",
+    version="1.0.0",
     author="Anicet Nougaret",
     author_email="anicet.nougaret@zohomail.eu",
     description="The first 2D python game engine rendering in the terminal.",
