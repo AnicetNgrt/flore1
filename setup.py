cc7f5793d15057e9823fed8f5139d15cc795dd6e
AnicetNgrt <an.nougaret@gmail.com>
2019-10-27 08:27:46 +0100

    Fixing demos, fixing pypI builds being empty (facepalm)

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ import setuptools
 
 setuptools.setup(
     name="flore1",
-    version="1.0.3",
+    version="1.0.4",
     author="Anicet Nougaret",
     author_email="anicet.nougaret@zohomail.eu",
     description="The first 2D python game engine rendering in the terminal.",
     url="https://github.com/AnicetNgrt/flore1",
-    install_requires=[            
+    packages=setuptools.find_packages(),
+    install_requires=[
           'pillow'
       ],
     classifiers=[
