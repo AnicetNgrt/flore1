3b0792b7ad7df53e30effb027ef2b30d4be27b67
Anicet Nougaret <anicet.nougaret@zohomail.eu>
2019-11-03 15:36:37 +0100

    Update tutorial4.py

--- demos/tutorial4/tutorial4.py
+++ demos/tutorial4/tutorial4.py
@@ -1,12 +1,12 @@
 import flore1
 
-Engine = flore1.Engine(auto_scale=True, win_mode=False)
+Graphics = flore1.Graphics(auto_scale=True, win_mode=False)
 Refresh = flore1.Refresh(fps=60)
 
-Scene = Engine.new_scene('main', res_x=100, res_y=100, coord_x=1, coord_y=1, layer_count=10)
+Scene = Graphics.new_scene('main', res_x=100, res_y=100, coord_x=1, coord_y=1, layer_count=10)
 
-explosion_sprt = Engine.new_sprite()
-explosion_fb = flore1.Flipbook(Engine, Refresh, explosion_sprt, path='assets/explosion/', size=[100, 100], transparent_rgb=(0, 0, 0), fps=60, sync=True)
+explosion_sprt = Graphics.new_sprite()
+explosion_fb = flore1.Flipbook(Graphics, Refresh, explosion_sprt, path='assets/explosion/', size=[100, 100], transparent_rgb=(0, 0, 0), fps=60, sync=True)
 Scene.put(explosion_sprt, 1, 1, 3)
 explosion_fb.start()
 
