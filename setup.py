12d72f4e62a6a6df37e5918275bb1323718ffb22
ANICET NOUGARET <nougaret@iut.univ-paris5.fr>
2019-11-12 08:58:20 +0100

    adding dependency

--- setup.py
+++ setup.py
@@ -3,15 +3,16 @@ import setuptools
 setuptools.setup(
     include_package_data=True,
     name="flore1",
-    version="1.5.2",
+    version="1.5.3",
     author="Anicet Nougaret",
     author_email="anicet.nougaret@zohomail.eu",
     description="The first 2D python game engine rendering in the terminal.",
     url="https://github.com/AnicetNgrt/flore1",
     packages=setuptools.find_packages(),
     install_requires=[
-          'pillow',
-	  'keyboard'
+        'pillow',
+        'keyboard',
+        'clipboard'
       ],
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
